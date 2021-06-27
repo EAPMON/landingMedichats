da2acce2fb9229b5822f493f94da8503c4209645
Eedal <elkindearmas2015@gmail.com>
2021-06-27 07:31:19 -0500

    a

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -348,6 +348,45 @@
         </div>
       </div>
     </section>
+    
+    
+  <section class="section section-lg pt-lg-1 section-contact-us">
+      <div class="container">
+        <div class="row justify-content-center mt--300">
+          <div class="col-lg-8">
+            <card gradient="secondary" shadow body-classes="p-lg-5">
+              <h4 class="mb-1">Want to work with us?</h4>
+              <p class="mt-0">Your project is very important to us.</p>
+              <base-input
+                class="mt-5"
+                alternative
+                placeholder="Your name"
+                addon-left-icon="ni ni-user-run"
+              >
+              </base-input>
+              <base-input
+                alternative
+                placeholder="Email address"
+                addon-left-icon="ni ni-email-83"
+              >
+              </base-input>
+              <base-input class="mb-4">
+                <textarea
+                  class="form-control form-control-alternative"
+                  name="name"
+                  rows="4"
+                  cols="80"
+                  placeholder="Type a message..."
+                ></textarea>
+              </base-input>
+              <base-button type="default" round block size="lg">
+                Send Message
+              </base-button>
+            </card>
+          </div>
+        </div>
+      </div>
+    </section>
   <!--  <section class="section bg-secondary">
       <div class="container">
         <div class="row row-grid align-items-center">
@@ -962,6 +1001,7 @@
         </div>
       </div>
     </section> -->
+    
     <a href="#" class="btn-flotante floating">
       <icon name="ni ni-bold-up">  </icon>
     </a>
