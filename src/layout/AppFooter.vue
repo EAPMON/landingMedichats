eedcc9fd33a86a6c2e0dc8d09931af68ea041a4e
Eedal <elkindearmas2015@gmail.com>
2021-06-27 17:16:41 -0500

    add google maps

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -3,17 +3,28 @@
         <div class="container container-lg">
             <div class="row">
                 <div class="col-md-6 mb-5 mb-md-0">
+                    <h1 class="display-3">
+                        ¿Cómo llegar?
+                    </h1>
+                    <br/>
                     <div class="card card-lift--hover shadow border-0">
-                        <router-link to="/landing" title="Landing Page">
-                            <img v-lazy="'img/theme/landing.jpg'" class="card-img">
-                        </router-link>
+                        <div class="card-body p-3" >
+                            <!--<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7848.672216138554!2d-75.515694!3d10.394857!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef625ed8b831a0f%3A0x5896a79f933f88a7!2sTv.%2049%20%2322%2C%20Cartagena%2C%20Provincia%20de%20Cartagena%2C%20Bol%C3%ADvar%2C%20Colombia!5e0!3m2!1ses-419!2sus!4v1624797718283!5m2!1ses-419!2sus" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>-->
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3924.3793752400657!2d-75.4839062825562!3d10.391412799999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef6256bec183dc9%3A0xd81f2596f6731ae4!2sComunidad%20jud%C3%ADa%20mesi%C3%A1nica%20Mz%208%20Lote%204!5e0!3m2!1ses-419!2sco!4v1624804965329!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-6 mb-5 mb-lg-0">
+                    <h1 class="display-3">
+                        ¿Casos de éxito?
+                    </h1>
+                    <br/>
                     <div class="card card-lift--hover shadow border-0">
-                        <router-link to="/profile" title="Profile Page">
-                            <img v-lazy="'img/theme/profile.jpg'" class="card-img">
-                        </router-link>
+                        <div class="card-body p-3">
+                        <iframe width="100%" height="450" src="https://www.youtube.com/embed/IaSIKN2ghx4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                        </div>
                     </div>
                 </div>
             </div>
